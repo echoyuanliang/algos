67750a6e055be0a00e7bee6c1251ced53cf2f010
zhangyuanliang <zhangyuanliang@baidu.com>
2018-06-07 23:16:56 +0800

    add loop permutation

--- pratice/permutation.go
+++ pratice/permutation.go
@@ -3,9 +3,10 @@ package pratice
 import (
     "fmt"
     "container/list"
+    "sort"
 )
 
-func permutation(arr []int, cursor int, end int) *list.List{
+func recursePermutation(arr []int, cursor int, end int) *list.List{
 
     permutationRes := list.New()
 
@@ -14,7 +15,7 @@ func permutation(arr []int, cursor int, end int) *list.List{
     }else{
         for i := cursor; i <= end; i++{
             arr[i], arr[cursor] = arr[cursor], arr[i]
-            permutationRes.PushBackList(permutation(arr, cursor+1, end))
+            permutationRes.PushBackList(recursePermutation(arr, cursor+1, end))
             arr[cursor], arr[i] = arr[i], arr[cursor]
         }
     }
@@ -22,6 +23,50 @@ func permutation(arr []int, cursor int, end int) *list.List{
     return permutationRes
 }
 
-func Permutation(arr []int) *list.List{
-    return permutation(arr, 0, len(arr) - 1)
+
+func RecursePermutation(arr []int) *list.List{
+    return recursePermutation(arr, 0, len(arr) - 1)
+}
+
+
+func reverse(arr []int){
+    i := 0
+    j := len(arr) -1
+
+    for i < j {
+        arr[i], arr[j] = arr[j], arr[i]
+        i++
+        j--
+    }
+}
+
+
+func LoopPermutation(arr []int) *list.List{
+
+    sort.Ints(arr)
+    arrLen := len(arr)
+    permutationRes :=list.New()
+
+    for{
+        permutationRes.PushBack(fmt.Sprint(arr))
+        i := arrLen - 2
+        for ; i >= 0; i--{
+            if arr[i] < arr[i+1] {
+                break
+            }else if i == 0{
+                return permutationRes
+            }
+        }
+
+        j := arrLen - 1
+        for ; j > i; j--{
+            if arr[j] > arr[i]{
+                break
+            }
+        }
+
+        arr[i], arr[j] = arr[j], arr[i]
+        reverse(arr[i+1:])
+    }
+
 }
--- pratice/pratice_test.go
+++ pratice/pratice_test.go
@@ -3,36 +3,61 @@ package pratice
 import (
     "testing"
     "math/rand"
+    "container/list"
+    "errors"
+    "fmt"
 )
-
 
 const TestCnt = 10
 
-func factorial(x int) int{
-    if x <= 2{
+func factorial(x int) int {
+    if x <= 2 {
         return x
     }
 
-    return x * factorial(x -1)
+    return x * factorial(x-1)
 }
 
-func TestPermutation(t *testing.T) {
-    randIntegers := rand.Perm(TestCnt)
-
-    expectedLen := factorial(len(randIntegers))
-
-    permRes := Permutation(randIntegers)
-    if permRes.Len() != expectedLen{
-        t.Fatal("permutation of", permRes.Len(), "must be ", expectedLen)
+func validatePermRes(res *list.List, expectedLen int) (error) {
+    if res.Len() != expectedLen {
+        return errors.New(fmt.Sprintf("permutation of %d must be %d", res.Len(), expectedLen))
     }
 
     permMap := make(map[string]interface{})
 
-    for permItem := permRes.Front(); permItem != nil; permItem = permItem.Next(){
+    for permItem := res.Front(); permItem != nil; permItem = permItem.Next() {
         permMap[permItem.Value.(string)] = nil
     }
 
-    if permRes.Len() != len(permMap){
-        t.Fatal("permutation is not unique")
+    if res.Len() != len(permMap) {
+        return errors.New(fmt.Sprintf("dumplicate permutation gen"))
+    }
+
+    return nil
+}
+
+func TestRecursePermutation(t *testing.T) {
+    randIntegers := rand.Perm(TestCnt)
+
+    expectedLen := factorial(TestCnt)
+
+    err := validatePermRes(RecursePermutation(randIntegers), expectedLen)
+
+    if err != nil {
+        t.Fatal(err)
     }
 }
+
+
+func TestLoopPermutation(t *testing.T) {
+    randIntegers := rand.Perm(TestCnt)
+
+    expectedLen := factorial(TestCnt)
+
+    err := validatePermRes(LoopPermutation(randIntegers), expectedLen)
+
+    if err != nil {
+        t.Fatal(err)
+    }
+}
+
