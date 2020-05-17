31c7e8bd0fe62850de5fe84dc0737996506967fb
zhangyuanliang <zhangyuanliang@baidu.com>
2020-05-17 16:17:09 +0800

    fix

--- leetcode/longest_sub_strings.go
+++ leetcode/longest_sub_strings.go
@@ -1,1 +1,32 @@
 package leetcode
+
+func LengthOfLongestSubstring(s string) int {
+
+    seeMap := make([]int, 256)
+
+    for idx := range seeMap {
+        seeMap[idx] = -1
+    }
+    maxLen := 0
+    curLen := 0
+    start := 0
+    for idx, c := range s {
+        if seeMap[c] >= start {
+            if curLen > maxLen {
+                maxLen = curLen
+            }
+
+            curLen = idx - seeMap[c]
+            start = seeMap[c] + 1
+        } else {
+            curLen += 1
+        }
+
+        seeMap[c] = idx
+    }
+
+    if curLen > maxLen {
+        maxLen = curLen
+    }
+    return maxLen
+}
--- leetcode/longest_sub_strings_test.go
+++ leetcode/longest_sub_strings_test.go
@@ -1,1 +1,25 @@
 package leetcode
+
+import "testing"
+
+var flagTests = []struct {
+    in  string
+    out int
+}{
+    {"''", 1},
+    {"abcdba", 4},
+    {"abcabcbb", 3},
+    {"bbbbb", 1},
+    {"pwwkew", 3},
+}
+
+func TestLengthOfLongestSubstring(t *testing.T) {
+
+    for _, tt := range flagTests {
+        res := LengthOfLongestSubstring(tt.in)
+
+        if res != tt.out {
+            t.Errorf("in %s, want %d, out %d", tt.in, tt.out, res)
+        }
+    }
+}
