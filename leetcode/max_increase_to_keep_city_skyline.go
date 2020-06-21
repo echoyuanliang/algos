50eac64aef42ae338c7b38a282d0dc63d456cf1b
zhangyuanliang <zhangyuanliang@baidu.com>
2020-06-21 18:50:58 +0800

    max-increase-to-keep-city-skyline

--- leetcode/max_increase_to_keep_city_skyline.go
+++ leetcode/max_increase_to_keep_city_skyline.go
@@ -1,1 +1,42 @@
 package leetcode
+
+// https://leetcode-cn.com/problems/max-increase-to-keep-city-skyline/
+
+func MaxIncreaseKeepingSkyline(grid [][]int) int {
+        rowMax := make([]int, len(grid))
+        colMax := make([]int, len(grid))
+
+        for row := 0; row < len(grid); row++ {
+                for col := 0; col < len(grid); col ++ {
+                        if grid[row][col] > rowMax[row] {
+                                rowMax[row] = grid[row][col]
+                        }
+
+                        if grid[row][col] > colMax[col] {
+                                colMax[col] = grid[row][col]
+                        }
+                }
+        }
+
+        inc := 0
+
+        for row := 0; row < len(grid); row++ {
+                for col := 0; col < len(grid); col++ {
+                        if grid[row][col] >= rowMax[row] || grid[row][col] >= colMax[col] {
+                                continue
+                        }
+
+                        maxHigh := rowMax[row]
+
+                        if maxHigh > colMax[col] {
+                                maxHigh = colMax[col]
+                        }
+
+                        inc += maxHigh - grid[row][col]
+
+                }
+
+        }
+
+        return inc
+}
--- leetcode/max_increase_to_keep_city_skyline_test.go
+++ leetcode/max_increase_to_keep_city_skyline_test.go
@@ -1,1 +1,31 @@
 package leetcode
+
+import (
+        "testing"
+)
+
+import (
+        "github.com/stretchr/testify/assert"
+)
+
+func TestMaxIncreaseKeepingSkyline(t *testing.T) {
+        tests := []struct {
+                Input [][]int
+                Want  int
+        }{
+                {
+                        Input: [][]int{
+                                {3, 0, 8, 4},
+                                {2, 4, 5, 7},
+                                {9, 2, 6, 3},
+                                {0, 3, 1, 0},
+                        },
+                        Want: 35,
+                },
+        }
+
+        for _, c := range tests {
+
+                assert.Equal(t, c.Want, MaxIncreaseKeepingSkyline(c.Input))
+        }
+}
